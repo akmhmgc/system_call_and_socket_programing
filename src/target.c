01aa60dc7e96ed77ec71e215a0746911b9ca8c5b
akmhmgc <akimuhamaguchi@icloud.com>
2025-11-02 23:03:19 +0900

    chore: add test framework

---
+++ src/target.c
@@ -0,0 +1,7 @@
+int function(const int a){
+    if (a > 10){
+        return 0;
+    } else {
+        return 1;
+    }
+}
---
+++ src/target.h
@@ -0,0 +1,6 @@
+# ifndef _TARGET_H_
+# define _TARGET_H_
+
+int function(int a);
+
+# endif /* _TARGET_H_ */
