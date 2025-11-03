813bcd075941e983a278cb4c74f635303d19cea7
akmhmgc <akimuhamaguchi@icloud.com>
2025-11-03 12:59:58 +0900

    chore: remove unused tests

--- src/target.c
+++
@@ -1,7 +0,0 @@
-int function(const int a){
-    if (a > 10){
-        return 0;
-    } else {
-        return 1;
-    }
-}
--- src/target.h
+++
@@ -1,6 +0,0 @@
-# ifndef _TARGET_H_
-# define _TARGET_H_
-
-int function(int a);
-
-# endif /* _TARGET_H_ */
